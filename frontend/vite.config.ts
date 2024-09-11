7c82d4da04378770d296f49b5194c2b0a95a7f3e
mindovermattr <dduim@bk.ru>
2024-09-11 03:13:41 +0300

    update config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,10 +6,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
-  build: {
-    assetsDir: 'assets',
-  },
+  base: '/vue-kanban/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
