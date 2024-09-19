167222aef36c6d33faf2eeaaecf0d4c243d05fca
Kuper <killkih@mail.ru>
2024-09-19 21:05:07 +0300

    #create #destroy

--- server/app/controllers/tasks_controller.rb
+++ server/app/controllers/tasks_controller.rb
@@ -6,10 +6,28 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(task_params)
 
+    if @task.save
+      render json: @task, status: :created, location: @task
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @task = Task.find_by(id: params[:id])
 
+    if @task
+      @task.destroy
+    else
+      render json: { error: 'Некорректный id' }, status: :bad_request
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :body, :category, :status, :period)
   end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  resource :task
+  resources :tasks, except: [:new, :show]
 end
