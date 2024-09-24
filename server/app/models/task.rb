4f3ee5b53987398640e0fde08700bc4dc31acfa8
Kuper <killkih@mail.ru>
2024-09-25 01:22:30 +0300

    validates for status

--- server/app/models/task.rb
+++ server/app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
+  STATUS_VALUES = ['noStatus', 'needDone', 'inProgress', 'testing', 'done']
+
   validates :name, :body, :period, :status, presence: true
+  validates :status, inclusion: { in: STATUS_VALUES }
 end
