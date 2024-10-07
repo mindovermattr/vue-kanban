552a3979878fb581a6b5f07dd217cc3f0bc73e29
Kuper <killkih@mail.ru>
2024-10-08 00:13:11 +0300

    routes + auth controllers

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }
+
   root to: 'tasks#index'
   resources :categories, except: [:new, :show]
   resources :tasks, except: [:new, :show]
