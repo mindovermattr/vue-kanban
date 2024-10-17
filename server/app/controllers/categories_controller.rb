d377d2c2e425ca64f06d2829dfd4ab0426a00b51
Kuper <killkih@mail.ru>
2024-10-17 20:57:40 +0300

    change others controllers

--- server/app/controllers/categories_controller.rb
+++ server/app/controllers/categories_controller.rb
@@ -1,34 +1,32 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_desk, only: [:index, :create]
+  before_action :set_category, only: [:update, :destroy]
 
   def index
-    @categories = Category.all
+    @categories = @desk.categories
     render json: @categories
   end
 
   def create
-    @category = Category.new(category_params)
+    @category = @desk.categories.new(category_params)
 
     if @category.save
-      render json: @category, status: :created, location: @category
+      render json: @category, status: :created
     else
       unprocessable_entity(@category)
     end
   end
 
   def update
-    @category = Category.find_by(id: params[:id])
-
     if @category.update(category_params)
-      render json: @category, status: :created, location: @category
+      render json: @category, status: :created
     else
       unprocessable_entity(@category)
     end
   end
 
   def destroy
-    @category = Category.find_by(id: params[:id])
-
     if @category
       @category.destroy
     else
@@ -37,6 +35,14 @@ class CategoriesController < ApplicationController
   end
 
   private
+
+  def set_desk
+    @desk = Desk.find_by(id: params[:desk_id])
+  end
+
+  def set_category
+    @category = Category.find_by(id: params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name, :main_color, :accent_color)
--- server/app/controllers/tasks_controller.rb
+++ server/app/controllers/tasks_controller.rb
@@ -1,34 +1,32 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_desk, only: [:index, :create]
+  before_action :set_task, only: [:update, :destroy]
 
   def index
-    @tasks = Task.all
+    @tasks = @desk.tasks
     render json: @tasks
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = @desk.tasks.new(task_params)
 
     if @task.save
-      render json: @task, status: :created, location: @task
+      render json: @task, status: :created
     else
       unprocessable_entity(@task)
     end
   end
 
   def update
-    @task = Task.find_by(id: params[:id])
-
     if @task.update(task_params)
-      render json: @task, status: :created, location: @task
+      render json: @task, status: :created
     else
       unprocessable_entity(@task)
     end
   end
 
   def destroy
-    @task = Task.find_by(id: params[:id])
-
     if @task
       @task.destroy
     else
@@ -37,6 +35,14 @@ class TasksController < ApplicationController
   end
 
   private
+
+  def set_desk
+    @desk = Desk.find_by(id: params[:desk_id])
+  end
+
+  def set_task
+    @task = Task.find_by(id: params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:name, :body, :category_id, :status, :period)
