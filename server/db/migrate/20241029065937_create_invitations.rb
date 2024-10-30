719986d2fa3710026dda16cc82786dd9b32c3463
Kuper <killkih@mail.ru>
2024-10-30 22:53:47 +0300

    change invites

--- server/db/migrate/20241029065937_create_invitations.rb
+++ server/db/migrate/20241029065937_create_invitations.rb
@@ -2,8 +2,9 @@ class CreateInvitations < ActiveRecord::Migration[7.1]
   def change
     create_table :invitations do |t|
       t.string :token, null: false
+      t.integer :max_uses, default: 1, null: false
+      t.integer :uses, default: 0, null: false
       t.references :desk, null: false, foreign_key: true
-      t.references :user, null: true, foreign_key: true # User who invited
       t.datetime :expires_at
 
       t.timestamps
