bf84061a732f2151bd52b3d8df3c7745b992f4be
Kuper <killkih@mail.ru>
2024-11-23 22:11:32 +0300

    + task_spec

---
+++ server/spec/models/task_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+
+  let(:task) { build(:task, period: Date.yesterday) }
+
+  it { should belong_to(:desk) }
+  it { should belong_to(:status) }
+  it { should belong_to(:category) }
+
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :body }
+  it { should validate_presence_of :period }
+
+  describe 'custom validations' do
+    it 'is invalid if period is in the past' do
+      task.validate
+      expect(task.errors[:period]).to include('must be in the future')
+    end
+  end
+end
