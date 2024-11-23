1e360fc4b46e47dde3c762bf99c5ca6b65f85e16
Kuper <killkih@mail.ru>
2024-11-23 21:29:34 +0300

    + desk_spec

---
+++ server/spec/models/desk_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Desk, type: :model do
+  it { should have_many(:tasks).dependent(:destroy) }
+  it { should have_many(:categories).dependent(:destroy) }
+  it { should have_many(:statuses).dependent(:destroy) }
+  it { should have_many(:desk_users).dependent(:destroy) }
+  it { should have_many(:users).through(:desk_users) }
+  it { should have_many(:invitations).dependent(:destroy) }
+
+  it { should validate_presence_of :name }
+end
